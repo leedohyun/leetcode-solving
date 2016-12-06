b59a30c6c1e31a3e6487d30dfc11321502e663a9
leedohyun <leedohyun@gmail.com>
2016-12-06 19:34:06 +0900

    https://leetcode.com/submissions/detail/84883078/

---
+++ src/main/java/kr/pe/leedohyun/sandbox/leetcode/dp/_152_MaximumProductSubarray.java
@@ -0,0 +1,31 @@
+package kr.pe.leedohyun.sandbox.leetcode.dp;
+
+/**
+ * [2, 3, -2, 4] -> [2, 3] = 6
+ * [-4, -3, -2, -1] -> [-4, -3, -2, -1] = 24
+ */
+public class _152_MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        int N = nums.length;
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int globalMax = nums[0];
+
+        for (int i = 1; i < N; i++) {
+            int tmpMax, tmpMin;
+            if (nums[i] > 0) {
+                tmpMax = Math.max(nums[i], nums[i] * currMax);
+                tmpMin = Math.min(nums[i], nums[i] * currMin);
+            } else {
+                tmpMax = Math.max(nums[i], nums[i] * currMin);
+                tmpMin = Math.min(nums[i], nums[i] * currMax);
+            }
+
+            currMax = tmpMax;
+            currMin = tmpMin;
+            globalMax = Math.max(globalMax, currMax);
+        }
+
+        return globalMax;
+    }
+}
---
+++ src/test/java/kr/pe/leedohyun/sandbox/leetcode/dp/_152_MaximumProductSubarrayTest.java
@@ -0,0 +1,18 @@
+package kr.pe.leedohyun.sandbox.leetcode.dp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _152_MaximumProductSubarrayTest {
+    _152_MaximumProductSubarray test = new _152_MaximumProductSubarray();
+
+    @Test
+    public void test() {
+        assertEquals(6, test.maxProduct(new int[]{2, 3, -2, 4}));
+        assertEquals(12, test.maxProduct(new int[]{-4, -3}));
+        assertEquals(24, test.maxProduct(new int[]{-4, -3, -2, -1}));
+        assertEquals(12, test.maxProduct(new int[]{-4, -3, 0, -2, -1}));
+        assertEquals(2, test.maxProduct(new int[]{2, -1, 1, 1}));
+    }
+}
