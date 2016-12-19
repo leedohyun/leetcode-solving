a00671fca5fb6a893c4650ec34bf008984536c33
leedohyun <leedohyun@gmail.com>
2016-12-19 19:53:09 +0900

    https://leetcode.com/submissions/detail/86109916/

---
+++ src/main/java/kr/pe/leedohyun/sandbox/leetcode/array/_001_TwoSum.java
@@ -0,0 +1,51 @@
+package kr.pe.leedohyun.sandbox.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _001_TwoSum {
+    /**
+     * O(N) solution
+     * 같은 숫자가 있어도 마지막으로 나오는 숫자가 저장되므로 2개 비교에 문제 없음
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        int N = nums.length;
+
+        Map<Integer, Integer> inverted = new HashMap<Integer, Integer>();
+        for (int i = 0; i < N; i++) {
+            int complement = target - nums[i];
+            if (inverted.containsKey(complement)) {
+                ret[0] = inverted.get(complement);
+                ret[1] = i;
+                break;
+            }
+            inverted.put(nums[i], i);
+        }
+        return ret;
+    }
+
+//    /**
+//     * O(N^2) solution
+//     */
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] ret = new int[2];
+//        int N = nums.length;
+//        for (int i = 0; i < N - 1; i++) {
+//            boolean isFind = false;
+//            for (int j = i + 1; j < N; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    ret[0] = i;
+//                    ret[1] = j;
+//                    isFind = true;
+//                    break;
+//                }
+//            }
+//
+//            if (isFind) {
+//                break;
+//            }
+//        }
+//        return ret;
+//    }
+}
---
+++ src/test/java/kr/pe/leedohyun/sandbox/leetcode/array/_001_TwoSumTest.java
@@ -0,0 +1,24 @@
+package kr.pe.leedohyun.sandbox.leetcode.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class _001_TwoSumTest {
+    _001_TwoSum test = new _001_TwoSum();
+
+    @Test
+    public void test() {
+        int[] result = test.twoSum(new int[]{2, 7, 11, 15}, 9);
+        assertEquals(0, result[0]);
+        assertEquals(1, result[1]);
+
+        result = test.twoSum(new int[]{3, 2, 4}, 6);
+        assertEquals(1, result[0]);
+        assertEquals(2, result[1]);
+
+        result = test.twoSum(new int[]{2, 1, 9, 4, 4, 56, 90, 3}, 8);
+        assertEquals(3, result[0]);
+        assertEquals(4, result[1]);
+    }
+}
