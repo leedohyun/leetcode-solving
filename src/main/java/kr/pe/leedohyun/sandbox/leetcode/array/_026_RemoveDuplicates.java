40a3677559db3f416ebd58d4413c30bb2289170c
leedohyun <leedohyun@gmail.com>
2016-12-21 19:30:05 +0900

    https://leetcode.com/submissions/detail/86324868/ (훨씬 간결한 코드)

--- src/main/java/kr/pe/leedohyun/sandbox/leetcode/array/_026_RemoveDuplicates.java
+++ src/main/java/kr/pe/leedohyun/sandbox/leetcode/array/_026_RemoveDuplicates.java
@@ -7,20 +7,32 @@ package kr.pe.leedohyun.sandbox.leetcode.array;
 public class _026_RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         int N = nums.length;
-        int count = N;
-        for (int i = 0; i < N - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i] = Integer.MIN_VALUE;
-                count--;
-            }
-        }
-
         int pos = 0;
-        for (int i = 0; i < N; i++) {
-            if (nums[i] != Integer.MIN_VALUE) {
-                nums[pos++] = nums[i];
+        for (int i = 1; i < N; i++) {
+            if (nums[pos] != nums[i]) {
+                pos++;
+                nums[pos] = nums[i];
             }
         }
-        return count;
+        return pos + 1;
     }
+
+//    public int removeDuplicates(int[] nums) {
+//        int N = nums.length;
+//        int count = N;
+//        for (int i = 0; i < N - 1; i++) {
+//            if (nums[i] == nums[i + 1]) {
+//                nums[i] = Integer.MIN_VALUE;
+//                count--;
+//            }
+//        }
+//
+//        int pos = 0;
+//        for (int i = 0; i < N; i++) {
+//            if (nums[i] != Integer.MIN_VALUE) {
+//                nums[pos++] = nums[i];
+//            }
+//        }
+//        return count;
+//    }
 }
